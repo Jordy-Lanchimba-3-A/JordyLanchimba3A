f23d5cb59bbf0bb11db05e90c417ca453ccc2fdd
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 21:08:48 -0500

    Add files via upload

---
+++ Ejercicio_5/Ejercicio_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número entero para crear una piramide: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int j = 0, k = 0;
+            for(int i = 1; i <= numero; i++)
+            {
+                for(j = 1; j <= (numero - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                for(k = 1; k <= i; k++)
+                {
+                    Console.Write('*');
+                }
+                for(k = (i - 1); k >= 1; k--)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
