cb1187a0699cb6e79f5d96549a208da2322873e3
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 20:52:42 -0500

    Add files via upload

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los de números que desea comparar: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            int[] Numeros = new int[valor];
+            for (int i = 0; i < valor; i++)
+            {
+                Numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int Mayor = Numeros.Max();
+            Console.WriteLine("El número mayor es: {0}", Mayor);
+            Console.ReadKey();
+        }
+    }
+}
