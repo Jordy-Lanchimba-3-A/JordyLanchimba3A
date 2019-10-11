d4821ddd079e699802ff0a0694dad092a7208e77
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 21:30:40 -0500

    Add files via upload

---
+++ Ejercicio_9/Ejercicio_9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número para generar una tabla de multiplicación:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+            int i = 1;
+            for (i = 1; i <= 15; i++)
+            {
+                Convert.ToInt32(numero);
+                Console.WriteLine("{0} x {1} = {2}", numero, i, numero * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
