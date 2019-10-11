18420d60e5cdc3e804084759b3d8a63da25b3a52
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 21:18:03 -0500

    Add files via upload

---
+++ Ejercicio_7/Ejercicio_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Dos columnas de números en orden ascendente (1 : 100) y descendente (100 : 1):");
+            int j = 100;
+            for (int i = 1; i <= j; i++)
+            {
+                Console.Write(i);
+                Console.Write("   ");
+                Console.WriteLine((j + 1) - i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
