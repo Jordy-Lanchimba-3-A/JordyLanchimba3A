a3d67e62c281c0f2976d0c8cb5c2d2c7bd2cbaef
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 21:09:54 -0500

    Add files via upload

---
+++ Ejercicio_6/Ejercicio_6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejercicio_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el número entero para hacer un rombo:");
+            int rombo = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= rombo; i++)
+            {
+                for (int j = 1; j <= rombo - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            for (int i = rombo - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= rombo - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
