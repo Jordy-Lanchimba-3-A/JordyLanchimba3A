6fc2550707594ca837c432a6c5dbf8d3deca531b
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 20:58:44 -0500

    Add files via upload

---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Producto = 0, Valor = 0, Acomulador = 0;
+            do
+            {
+                Console.WriteLine("Ingrese cantidad vendida: ");
+                Producto = Convert.ToInt32(Console.ReadLine());
+                if (Producto != 0)
+                {
+                    Console.WriteLine("Ingrese el precio del producto: ");
+                    Valor = Convert.ToInt32(Console.ReadLine());
+                }
+                Acomulador = Acomulador + (Valor * Producto);
+            } while (Producto != 0);
+            Console.WriteLine("El valor a pagar es: {0}", Acomulador);
+            Console.ReadKey();
+        }
+    }
+}
