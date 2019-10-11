1873f1f4bf9555690ec8ffc3d25a9c2dcf284eac
Jordy-Lanchimba-3-A <56418626+Jordy-Lanchimba-3-A@users.noreply.github.com>
2019-10-10 21:23:36 -0500

    Add files via upload

---
+++ Ejercicio_8/Ejercicio_8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número entero para empezar el conteo:");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            int Doble = 0, Triple = 0;
+            Console.WriteLine(" ");
+            for (int i = 1; i <= Num; i++)
+            {
+                
+                Console.Write(i);
+                Console.Write(" ");
+                Doble = Doble + 2;
+                if (Doble > Num)
+                {
+                    Console.Write("  ");
+                }
+                else
+                {
+                    Console.Write(Doble);
+                }
+                Console.Write("  ");
+                Triple = Triple + 3;
+                if (Triple > Num)
+                {
+                    Console.Write("  ");
+                }
+                else
+                {
+                    Console.Write(Triple);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
